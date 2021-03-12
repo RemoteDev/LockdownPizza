4a28bb73f8b235cca86aa9fe22d0907fb1d46ed1
KyleBaxter9 <u1858956@unimail.hud.ac.uk>
2021-03-12 14:19:37 +0000

    Updated

--- sign_up_auth.php
+++ sign_up_auth.php
@@ -42,34 +42,5 @@ if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['first_n
 			header("Location: login.php?success=Successfully Registered! Please Login");
 		}
 
-		
-		// $stmt = $conn->prepare('SELECT * FROM users WHERE email=?');
-		// $stmt->execute([$email]);
-
-		// if ($stmt->rowCount() === 1) {
-		// 	$user = $stmt->fetch();
-            
-		// 	$user_id = $user['id'];
-		// 	$user_email = $user['email'];
-		// 	$user_password = $user['password'];
-		// 	$user_full_name = $user['full_name'];
-
-		// 	if ($email === $user_email) {
-        //         header("Location: index.php");
-		// 		if (password_verify($password, $user_password)) {
-		// 			$_SESSION['user_id'] = $user_id;
-		// 			$_SESSION['user_email'] = $user_email;
-		// 			$_SESSION['user_full_name'] = $user_full_name;
-		// 			header("Location: index.php");
-
-		// 		}else {
-		// 			header("Location: signup.php?error=Incorect User name or password");
-		// 		}
-		// 	}else {
-		// 		header("Location: signup.php?error=Incorect User name or password");
-		// 	}
-		// }else {
-		// 	header("Location: signup.php?error=Incorect User name or password");
-		// }
 	}
 }
